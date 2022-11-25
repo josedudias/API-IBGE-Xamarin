376002e358b28b3f19674def3dff6ac32951d4db
José Eduardo Dias <jose.rufino01@etec.sp.gov.br>
2022-11-25 17:15:57 +0000

    Add project files.

---
+++ API IBGE/API IBGE/Model/Post.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace API_IBGE.Model
+{
+    public class Post : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+
+        private string _title;
+        [JsonProperty("title")]
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
